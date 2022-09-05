bde16f899cb1655823f607aa1f8ebb981414f4bd
Gary A. Stafford <gary.a.stafford@gmail.com>
2022-09-04 23:00:31 -0400

    Working Total to Kafka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,54 +16,39 @@ import org.example.schema.TotalSerializationSchema;
 
 import java.time.LocalDateTime;
 
-
 public class Main {
+
+    static String BOOTSTRAP_SERVERS = "kafka:29092";
+    static String CONSUMER_GROUP_ID = "demo_group";
+    static String INPUT_TOPIC = "demo.purchases";
+    static String OUTPUT_TOPIC = "demo.totals";
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Apache Flink!");
-        ReadKafka();
+        FlinkKafkaPipeline();
     }
 
-
-    public static void ReadKafka() throws Exception {
+    public static void FlinkKafkaPipeline() throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         KafkaSource<Purchase> source = KafkaSource.<Purchase>builder()
-                .setBootstrapServers("kafka:29092")
-                .setTopics("demo.purchases")
-                .setGroupId("test_group")
+                .setBootstrapServers(BOOTSTRAP_SERVERS)
+                .setTopics(INPUT_TOPIC)
+                .setGroupId(CONSUMER_GROUP_ID)
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new PurchaseDeserializationSchema())
                 .build();
 
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-
         DataStream<Purchase> streamSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-//        KeyedStream<Purchase, String> keyedStream = streamSource.keyBy(value -> value.productId);
-//
-//        DataStream<Tuple3<String, Integer, Float>> totals =
-//                streamSource.flatMap((FlatMapFunction<Purchase, Tuple3<String, Integer, Float>>) (value, out) ->
-//                        out.collect(new Tuple3<>(value.getProductId(), value.getQuantity(), value.getTotalPurchase())));
-
-
-//        DataStream<Tuple3<String, Integer, Float>> runningTotals = streamSource
-//                .flatMap((FlatMapFunction<Purchase, Tuple3<String, Integer, Float>>) (value, out) -> out.collect(new Tuple3<>(value.getProductId(), value.getQuantity(), value.getTotalPurchase())))
-//                .keyBy(value -> value.f0)
-//                .reduce((d1, d2) -> {
-//                    d1.f1 += d2.f1;
-//                    d1.f2 += d2.f2;
-//                    return d1;
-//                });
-
         DataStream<Total> totals = streamSource.flatMap(
-                (FlatMapFunction<Purchase, Total>) (value, out) -> out.collect(
-                        new Total(
-                                LocalDateTime.now().toString(),
-                                value.getProductId(),
-                                value.getQuantity(),
-                                value.getTotalPurchase()
-                        ))).returns(Total.class);
+                (FlatMapFunction<Purchase, Total>) (value, out) -> out.collect(new Total(
+                        LocalDateTime.now().toString(),
+                        value.getProductId(),
+                        value.getQuantity(),
+                        value.getTotalPurchase()
+                ))).returns(Total.class);
 
         DataStream<Total> runningTotalsGeneric = totals
                 .keyBy(Total::getProductId)
@@ -73,52 +58,19 @@ public class Main {
                     return t1;
                 });
 
-
-//        KafkaSink<Tuple3<String, Integer, Float>> sink = KafkaSink.<Tuple3<String, Integer, Float>>builder()
-//                .setBootstrapServers("kafka:29092")
-//                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
-//                        .setTopic("demo.totals")
-//                        .setValueSerializationSchema(new SerializationSchema<Tuple3<String, Integer, Float>>() {
-//                            @Override
-//                            public void open(InitializationContext context) throws Exception {
-//                                SerializationSchema.super.open(context);
-//                            }
-//
-//                            @Override
-//                            public byte[] serialize(Tuple3<String, Integer, Float> element) {
-//                                NumberFormat formatter = new DecimalFormat("0.00");
-//                                String message = String.format("%s: %s: %s", element.f0, element.f1, formatter.format(element.f2));
-//                                return message.getBytes();
-//                            }
-//                        })
-//                        .build()
-//                )
-//                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-//                .build();
-
         KafkaSink<Total> sink = KafkaSink.<Total>builder()
-                .setBootstrapServers("kafka:29092")
+                .setBootstrapServers(BOOTSTRAP_SERVERS)
                 .setRecordSerializer(KafkaRecordSerializationSchema.builder()
-                        .setTopic("demo.output")
+                        .setTopic(OUTPUT_TOPIC)
                         .setValueSerializationSchema(new TotalSerializationSchema())
                         .build()
                 )
                 .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
                 .build();
 
-//        KafkaSink<Purchase> sink = KafkaSink.<Purchase>builder()
-//                .setBootstrapServers("kafka:29092")
-//                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
-//                        .setTopic("demo.output")
-//                        .setValueSerializationSchema(new PurchaseSerializationSchema())
-//                        .build()
-//                )
-//                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-//                .build();
-
         runningTotalsGeneric.sinkTo(sink);
 
-        env.execute("Kafka Flink Demo");
+        env.execute("Flink Kafka Pipeline");
 
     }
 }
